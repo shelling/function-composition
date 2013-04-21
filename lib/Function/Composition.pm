5b7fa14d2f3b33ea85270984a110098acc8ee6ae
shelling <navyblueshellingford@gmail.com>
2013-04-22 00:42:22 +0800

    initial commit

---
+++ lib/Function/Composition.pm
@@ -0,0 +1,32 @@
+package Function::Composition;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Function::Composition -
+
+=head1 SYNOPSIS
+
+  use Function::Composition;
+
+=head1 DESCRIPTION
+
+Function::Composition is
+
+=head1 AUTHOR
+
+shelling E<lt>navyblueshellingford@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
